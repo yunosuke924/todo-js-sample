b0e280914d69c8369c0f9090a38617c77ba2a973
Yunosuke Minegishi <72934392+yunosuke924@users.noreply.github.com>
2021-09-06 00:08:22 +0900

    コメントアウトの編集

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "./styles.css";
 
 const onClickAdd = () => {
-  // テキストボックスの値を取得し、初期化する
+  // テキストボックスの値を取得し初期化する
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
   createIncompleteList(inputText);
